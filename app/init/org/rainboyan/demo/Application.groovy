d9257a4c4fa4bed01261933e030369ba6b6b0a1b
Michael Yan <rain@rainboyan.com>
2023-11-23 14:56:53 +0800

    Upgrade to GoBoots 2024.0.0-M4

--- app/init/org/rainboyan/demo/Application.groovy
+++ app/init/org/rainboyan/demo/Application.groovy
@@ -1,10 +1,15 @@
 package org.rainboyan.demo
 
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration
+
 import grails.boot.Grails
 
 import groovy.transform.CompileStatic
 
 @CompileStatic
+@SpringBootApplication(exclude = [DataSourceAutoConfiguration, LiquibaseAutoConfiguration])
 class Application {
 
     static void main(String[] args) {
